678b7e948dfdbd5e4dbd7dcb5db9ae73b70d10bf
Mark Sayson <masayson@gmail.com>
2016-02-03 09:31:14 -0800

    Added run-length-encoding with position

--- patternMatching/countInstances.erl
+++ patternMatching/countInstances.erl
@@ -1,6 +1,9 @@
 -module(countInstances).
 
--export [runLengthEncode/1, addToCounts/2, addToConsecutiveCounts/2].
+-export [
+      runLengthEncode/1, runLengthEncodeWithPos/1,
+      addToCounts/2, addToConsecutiveCounts/2, addToConsecCountsWithPos/3
+   ].
 
 % Encodes elements of list as a compressed list of tuples
 % {Item, Count} representing the number of consecutive
@@ -14,6 +17,19 @@ runLengthEncode(L) when is_list(L) ->
 rle([], Counts) -> Counts;
 rle([H|T], Counts) ->
   rle(T, addToConsecutiveCounts(H, Counts)).
+
+% Run-length encoding with start positions of each run
+%
+% Examples:
+% runLengthEncodeWithPos([]) -> []
+% runLengthEncodeWithPos([a,a,a]) -> [{a,3,0}]
+% runLengthEncodeWithPos([1,2,2,2,3,3,1]) -> [{1,1,0},{2,3,1},{3,2,4},{1,1,6}]
+runLengthEncodeWithPos(L) when is_list(L) ->
+  rlep(L, [], 0).
+
+rlep([], Counts, _) -> Counts;
+rlep([H|T], Counts, Pos) ->
+  rlep(T, addToConsecCountsWithPos(H, Counts, Pos), Pos+1).
 
 % Adds an item to a list encoding the total number of
 % appearances of each item in the list.
@@ -39,3 +55,18 @@ addToConsecutiveCounts(Item, [{Item, Count}]) ->
   [{Item, Count + 1}];
 addToConsecutiveCounts(Item, [H|T]) ->
   [H|addToConsecutiveCounts(Item, T)].
+
+% Adds an item to a list encoding the number of
+% consecutive appearances of each item in the list
+% and the starting position of each run.
+%
+% Examples:
+% addToConsecCountsWithPos(a, [], 0) -> [{a, 1, 0}]
+% addToConsecCountsWithPos(a, [{a, 1, 0}], 1) -> [{a, 2, 0}]
+% addToConsecCountsWithPos(b, [{a, 2, 0}], 2) -> [{a, 2, 0}, {b, 1, 2}]
+
+addToConsecCountsWithPos(Item, [], Pos) -> [{Item, 1, Pos}];
+addToConsecCountsWithPos(Item, [{Item, Count, Pos}], _) ->
+  [{Item, Count + 1, Pos}];
+addToConsecCountsWithPos(Item, [H|T], Pos) ->
+  [H|addToConsecCountsWithPos(Item, T, Pos)].
--- patternMatching/countInstances_test.erl
+++ patternMatching/countInstances_test.erl
@@ -1,6 +1,9 @@
 -module(countInstances_test).
 
--import(countInstances, [runLengthEncode/1, addToCounts/2, addToConsecutiveCounts/2]).
+-import(countInstances, [
+      runLengthEncode/1, runLengthEncodeWithPos/1,
+      addToCounts/2, addToConsecutiveCounts/2, addToConsecCountsWithPos/3
+   ]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -9,6 +12,12 @@ runLengthEncode_test() ->
       ?assertEqual([{a, 1}], runLengthEncode([a])),
       ?assertEqual([{a, 1}, {b, 1}, {c, 1}, {b, 2}], runLengthEncode([a, b, c, b, b])),
       ?assertEqual([{5, 2}, {1, 1}, {-7, 1}, {1, 1}], runLengthEncode([5, 5, 1, -7, 1]))
+   ].
+
+runLengthEncodeWithPos_test() ->
+   [  ?assertEqual([], runLengthEncodeWithPos([])),
+      ?assertEqual([{a,3,0}], runLengthEncodeWithPos([a,a,a])),
+      ?assertEqual([{a,2,0}, {b,3,2}, {a,1,5}], runLengthEncodeWithPos([a,a,b,b,b,a]))
    ].
 
 addToCounts_test() ->
@@ -23,4 +32,11 @@ addToConsecutiveCounts_test() ->
       ?assertEqual([{a, 6}], addToConsecutiveCounts(a, [{a, 5}])),
       ?assertEqual([{b, 3}, {a, 1}], addToConsecutiveCounts(a, [{b, 3}])),
       ?assertEqual([{a, 2}, {b, 5}, {a, 1}], addToConsecutiveCounts(a, [{a, 2}, {b, 5}]))
+   ].
+
+addToConsecCountsWithPos_test() ->
+   [  ?assertEqual([{a, 1, 0}], addToConsecCountsWithPos(a, [], 0)),
+      ?assertEqual([{a, 6, 3}], addToConsecCountsWithPos(a, [{a, 5, 3}], 0)),
+      ?assertEqual([{a, 2, 4}, {b, 1, 6}], addToConsecCountsWithPos(b, [{a, 2, 4}], 6)),
+      ?assertEqual([{a, 2, 0}, {b, 1, 2}, {a, 1, 123}], addToConsecCountsWithPos(a, [{a, 2, 0}, {b, 1, 2}], 123))
    ].
